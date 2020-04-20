073ca00abebe35a4caa1d44646b7760135b0ceb9
muddy-blunts <55998224+muddy-blunts@users.noreply.github.com>
2020-04-21 01:38:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import telepot
+import telebot
 
-bot = telepot.Bot('1082662537:AAEVzaJsdns5UxsM12O-09oKzQII0mW9ZCw')
+bot = telebot.Bot('1082662537:AAEVzaJsdns5UxsM12O-09oKzQII0mW9ZCw')
 bot.sendMessage(828091313, 'Я перезагрузился!')
